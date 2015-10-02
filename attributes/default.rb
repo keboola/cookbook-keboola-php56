9b2deee70f778177bd32165554163c90a045a0e1
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2015-10-02 12:12:36 +0800

    Increase PHP memory limit

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,6 @@
 default['php']['packages'] = %w{ php56 php56-opcache php56-devel php-pear php56-pdo php56-mysqlnd php55-mbstring php56-mcrypt }
 default['php']['directives'] = {
     'date.timezone' => 'Europe/Prague',
-    'always_populate_raw_post_data' => '-1'
+    'always_populate_raw_post_data' => '-1',
+    'memory_limit' => '256M'
 }
